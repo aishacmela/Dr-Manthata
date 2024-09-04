cfde3e1899cc561bbfea5945d1195a0278fd5348
Aishacmela <aishacmela@gmail.com>
2024-09-04 12:10:27 +0200

    added Home component and added router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from "./Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Home";
+import Move from "./Move";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,54 @@
+import React from "react";
+import blackFamily from "./img/gallery/blackfamily.png";
+import heroBg from "./img/gallery/hero-bg.png";
+
+function Home() {
+  return (
+    <section className="py-xxl-10 pb-0" id="home">
+      <div
+        className="bg-holder bg-size"
+        style={{
+          backgroundImage: `url(${heroBg})`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      ></div>
+      {/*/.bg-holder*/}
+
+      <div className="container">
+        <div className="row min-vh-xl-100 min-vh-xxl-25">
+          <div className="col-md-5 col-xl-6 col-xxl-7 order-0 order-md-1 text-end">
+            <img
+              className="pt-7 pt-md-0 w-100"
+              src={blackFamily}
+              alt="hero-header"
+            />
+          </div>
+          <div className="col-md-75 col-xl-6 col-xxl-5 text-md-start text-center py-6">
+            <h1 className="fw-light font-base fs-6 fs-xxl-7">
+              We're <strong>determined</strong> for
+              <br />
+              your&nbsp;<strong>better life.</strong>
+            </h1>
+            <p className="fs-1 mb-5">
+              You can get the care you need 24/7 – be it online or in
+              <br />
+              person. You will be treated by a caring specialist doctor.
+              <br />
+              Voted the best doctor in Mankweng by patients and residents.
+            </p>
+            <a
+              className="btn btn-lg btn-primary rounded-pill"
+              href="#!"
+              role="button"
+            >
+              Make an Appointment
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
